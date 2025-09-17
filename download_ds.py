f5f05b3bc04af1d2f2050538521ccf99dd9243d9
akash <akashgoyal7@gmail.com>
2025-09-17 22:57:05 +0530

    [update] retrieval datasets and updated download code

--- download_ds.py
+++ download_ds.py
@@ -1,5 +1,29 @@
+import os
 from datasets import load_dataset
 
-# Download SciDocs
-dataset = load_dataset("mteb/scidocs", "default")
-dataset.save_to_disk("./mteb_data/scidocs")
+ds_map = {
+    "ArguAna": "mteb/arguana",
+    "CQADupstackRetrieval": "mteb/cqadupstack-retrieval",
+    "ClimateFEVER": "mteb/climate-fever",
+    "DBPedia": "mteb/dbpedia",
+    "FEVER": "mteb/fever",
+    # "FiQA2018": "mteb/FiQA2018-NL",
+    "HotpotQA": "mteb/hotpotqa",
+    "MSMARCO": "mteb/msmarco",
+    "NFCorpus": "mteb/nfcorpus",
+    "NQ": "mteb/nq",
+    # "Quora-NL": "mteb/Quora-NL",
+    "SCIDOCS": "mteb/scidocs",
+    "SciFact": "mteb/scifact",
+    "TRECCOVID": "mteb/trec-covid",
+    "Touche2020": "mteb/touche2020"
+}
+
+# Download datasets
+for k,v in ds_map.items():
+    print(f"Downloading for Task:{k} Dataset:{v}")
+    output_path = f"./mteb_datasets/{k}"
+    if not os.path.exists(output_path):
+        dataset = load_dataset(f"{v}", "default")
+        dataset.save_to_disk(output_path)
+    
--- main_2.py
+++ main_2.py
@@ -2,17 +2,35 @@ from datasets import load_from_disk
 from sentence_transformers import SentenceTransformer
 import mteb 
 
+#
+ds_map = {
+    "ArguAna": "mteb/arguana",
+    "CQADupstackRetrieval": "mteb/cqadupstack-retrieval",
+    "ClimateFEVER": "mteb/climate-fever",
+    "DBPedia": "mteb/dbpedia",
+    "FEVER": "mteb/fever",
+    # "FiQA2018": "mteb/FiQA2018-NL",
+    "HotpotQA": "mteb/hotpotqa",
+    "MSMARCO": "mteb/msmarco",
+    "NFCorpus": "mteb/nfcorpus",
+    "NQ": "mteb/nq",
+    # "Quora-NL": "mteb/Quora-NL",
+    "SCIDOCS": "mteb/scidocs",
+    "SciFact": "mteb/scifact",
+    "TRECCOVID": "mteb/trec-covid",
+    "Touche2020": "mteb/touche2020"
+}
 
 #
 model_name = "all-MiniLM-L6-v2"
 model = SentenceTransformer(model_name)
 # Select tasks
-tasks = mteb.get_tasks(tasks=["SCIDOCS"])
+tasks = mteb.get_tasks(tasks=list(ds_map.keys()))
 evaluation = mteb.MTEB(tasks=tasks)
 # Force dataset to come from local path
 for task in evaluation.tasks:
     print(task.metadata.name)
-    # task.dataset = load_from_disk(f"./mteb_data/{task.metadata.name}")
-    task.dataset = load_from_disk(f"./mteb_data/scidocs")
+    task.dataset = load_from_disk(f"./mteb_datasets/{task.metadata.name}")
+    # task.dataset = load_from_disk(f"./mteb_data/scidocs")
 # Run evaluation
 evaluation.run(model, output_folder="./results")
